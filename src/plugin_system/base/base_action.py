14fe86d93875ca2ea88d7f24405231f85b10493b
tt-P607 <68868379+tt-P607@users.noreply.github.com>
2025-09-07 13:14:48 +0800

    refactor(plugin_system): 移除未使用的 ChatType 导入

--- src/plugin_system/base/base_action.py
+++ src/plugin_system/base/base_action.py
@@ -6,7 +6,7 @@ from typing import Tuple, Optional
 
 from src.common.logger import get_logger
 from src.chat.message_receive.chat_stream import ChatStream
-from src.plugin_system.base.component_types import ActionActivationType, ChatMode, ActionInfo, ComponentType, ChatType,
+from src.plugin_system.base.component_types import ActionActivationType, ChatMode, ActionInfo, ComponentType, ChatType
 from src.plugin_system.apis import send_api, database_api, message_api
 
 
